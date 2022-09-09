5af18855a9342e32377232d8ab5c360613f192cf
xlostpexz <98389349+xlostpexz@users.noreply.github.com>
2022-09-09 17:45:12 +0700

    Update and rename README.md to legendfriend.lua

---
+++ legendfriend.lua
@@ -0,0 +1,15 @@
+_G.A = true
+
+spawn(function()
+       game:GetService("RunService").RenderStepped:Connect(function()
+        pcall(function()
+            if _G.A then
+game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(123.56916, 13.0345058, -384.393646, 0.48280865, 5.76925423e-08, -0.875725865, 2.32441622e-08, 1, 7.86947467e-08, 0.875725865, -5.83500182e-08, 0.48280865)
+fireclickdetector(game:GetService("Workspace")["Sell Box"].Box.ClickDetector)
+    if game.Players.LocalPlayer.Character.RightHand.JobBox then
+game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(121.356522, 12.8199968, -250.877472, -0.130248696, -6.63073791e-08, 0.991481364, 8.43981951e-09, 1, 6.79858019e-08, -0.991481364, 1.72229857e-08, -0.130248696)fireclickdetector(game:GetService("Workspace")["Sell Box"].Box.ClickDetector)
+end
+end
+end)
+end)
+end)
