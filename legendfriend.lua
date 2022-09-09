207d6db666784be57b3a286f0c8e963314595af1
xlostpexz <98389349+xlostpexz@users.noreply.github.com>
2022-09-09 17:49:43 +0700

    Update legendfriend.lua

--- legendfriend.lua
+++ legendfriend.lua
@@ -1,9 +1,7 @@
-_G.A = true
-
 spawn(function()
        game:GetService("RunService").RenderStepped:Connect(function()
         pcall(function()
-            if _G.A then
+            if _G.AutoFarm then
 game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(123.56916, 13.0345058, -384.393646, 0.48280865, 5.76925423e-08, -0.875725865, 2.32441622e-08, 1, 7.86947467e-08, 0.875725865, -5.83500182e-08, 0.48280865)
 fireclickdetector(game:GetService("Workspace")["Sell Box"].Box.ClickDetector)
     if game.Players.LocalPlayer.Character.RightHand.JobBox then
